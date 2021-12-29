623e07c912cc30c666fd310436ee7e7e156109a1
t5thr <burykina20025@mail.ru>
2021-12-29 14:03:35 +0300

    table

--- data.sql
+++ data.sql
@@ -1,3 +1,17 @@
-select * from mars;
+-- insert into mars (first_name, middle_name, last_name, email) values
+--('Иван','Иванович', 'Иванов', 'ivanov@mail.ru'),
+--('Петр','Петрович','Петров','petrov@.mail.ru'),
+--('Иван','Петрович','Сидоров','sidorov@mail.ru'),
+--('Михаил','Викторович','Кравченко','kravchenko@mail.ru'),
+--('Александр','Сергеевич','Арсеньев','aleks234@mail.ru'),
+--('Святослав','Григориевич','Казаков','kazakov@mail.ru'),
+--('Олег','Алексеевич','Головко','golovkooleg@mail.ru'),
+--('Максим','Александрович','Мирошин','miroshinmaksim@mail.ru'),
+--('Кирилл','Михайлович','Андреев','andreevkirill23@mail.ru'),
+--('Даниил','Сергеевич','Коробко','korobkod@mail.ru'),
+--('Владимир','Александрович','Меньшов','vladimirm@mail.ru'),
+--('Дмитрий','Константинович','Козловский','kozlovskiyd@mail.ru'),
+--('Сергей','Викторович','Глухов','gluhovs@mail.ru');
 
+--select * from mars;
 
